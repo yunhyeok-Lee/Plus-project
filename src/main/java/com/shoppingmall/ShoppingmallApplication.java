d618f536bab6b57f5c43a285ce9b2f8e00609eb6
yunhyeok_lee <yuy9611@gmail.com>
2025-05-21 20:51:14 +0900

    Revert "feat : build.gradle QueryDSL 추가"
    This reverts commit f2152043b80a495ebfabb3ce9cc534a662308539.

--- src/main/java/com/shoppingmall/ShoppingmallApplication.java
+++ src/main/java/com/shoppingmall/ShoppingmallApplication.java
@@ -2,10 +2,8 @@ package com.shoppingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class ShoppingmallApplication {
 
 	public static void main(String[] args) {
